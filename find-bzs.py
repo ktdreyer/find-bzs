95ba02e87455ac1ef163369d250c8d8e1c83e40c
Ken Dreyer <kdreyer@redhat.com>
2017-10-31 09:28:19 -0600

    new ceph-ansible versions

--- find-bzs.py
+++ find-bzs.py
@@ -20,8 +20,8 @@ PRODUCT = 'Red Hat Ceph Storage'
 # These are ceph-ansible tags to compare:
 # TODO: auto-determine "OLD" from ceph-3.0-rhel-7-candidate
 # TODO: auto-determine "NEW" from git-decribe
-OLD = 'v3.0.3'
-NEW = 'v3.0.4'
+OLD = 'v3.0.7'
+NEW = 'v3.0.8'
 
 
 def github_project():
