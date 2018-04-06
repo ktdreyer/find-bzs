6948dfd34b19e5ac2cf6317ca908c16c7c5a0654
Ken Dreyer <kdreyer@redhat.com>
2018-04-05 21:39:42 -0600

    new ceph-ansible v3.0.29

--- find-bzs.py
+++ find-bzs.py
@@ -17,8 +17,8 @@ Find Bugzilla numbers that correlate to new upstream tags in GitHub.
 # These are ceph-ansible tags to compare:
 # TODO: auto-determine "OLD" from ceph-3.0-rhel-7-candidate
 # TODO: auto-determine "NEW" from git-decribe
-OLD = 'v3.0.27'
-NEW = 'v3.0.28'
+OLD = 'v3.0.28'
+NEW = 'v3.0.29'
 
 GITHUBURL = 'https://github.com/'
 GITHUBAPI = 'https://api.github.com/'
