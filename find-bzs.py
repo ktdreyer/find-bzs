ktdreyer/find-bzs

commit 76151791c70a6d85a5e6b4ac930299488069866f

new ceph-ansible 3.1.0rc11

find-bzs.py
@@ -17,8 +17,8 @@ Find Bugzilla numbers that correlate to new upstream tags in GitHub.
 # These are ceph-ansible tags to compare:
 # TODO: auto-determine "OLD" from ceph-3.0-rhel-7-candidate
 # TODO: auto-determine "NEW" from git-decribe
-OLD = 'v3.1.0rc9'
-NEW = 'v3.1.0rc10'
+OLD = 'v3.1.0rc10'
+OLD = 'v3.1.0rc11'
 
 GITHUBURL = 'https://github.com/'
 GITHUBAPI = 'https://api.github.com/'

commit 9e1295ff9dccbaf0024b652f04c0c701fdbb6267

fix NEW variable
broken in 76151791c70a6d85a5e6b4ac930299488069866f, whoops

find-bzs.py
@@ -18,7 +18,7 @@ Find Bugzilla numbers that correlate to new upstream tags in GitHub.
 # TODO: auto-determine "OLD" from ceph-3.0-rhel-7-candidate
 # TODO: auto-determine "NEW" from git-decribe
 OLD = 'v3.1.0rc10'
-OLD = 'v3.1.0rc11'
+NEW = 'v3.1.0rc11'
 
 GITHUBURL = 'https://github.com/'
 GITHUBAPI = 'https://api.github.com/'